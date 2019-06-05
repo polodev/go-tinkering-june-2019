222a9b8e90533088755164591c189b5417b1cb8d
Polo Dev <polodev10@gmail.com>
2019-06-05 13:21:26 +0600

    added loops:

--- 06_conditional/main.go
+++ 06_conditional/main.go
@@ -1,7 +1,27 @@
-package main;
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	x := 15
+	y := 10
+
+	// if
+	if x < y {
+		fmt.Printf("%d is less than %d", x, y)
+	} else {
+		fmt.Printf("%d is less than %d", y, x)
+	}
+
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("color is red")
+	} else if color == "green" {
+		fmt.Println("color is green")
+	} else {
+		fmt.Println("different color")
+	}
+
+	// switch - same as other languages
 }
---
+++ 07_loops/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Long method
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		// i = i + 1
+		i++
+	}
+
+	// Short method
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("Number %d\n", i)
+	}
+}
