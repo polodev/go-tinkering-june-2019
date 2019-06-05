b3e92e5dfffc4d76e5a34f382d6972083081b3d7
Polo Dev <polodev10@gmail.com>
2019-06-05 12:57:34 +0600

    added functions

---
+++ 03_packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/polodev/go_crash_course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutil.Reverse("olleh"))
+}
